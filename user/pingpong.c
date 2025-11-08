0260426f273fb6f6c021a8cda01875be7908e829
LBZ <2751821409@qq.com>
2025-11-08 20:15:23 +0800

    pingpong_forget to switch branch

---
+++ user/pingpong.c
@@ -0,0 +1,42 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(void) {
+    int p1[2], p2[2];
+    char buf[1];
+    pipe(p1); pipe(p2);
+    if (fork() == 0)
+    {
+        close(p1[1]);
+        close(p2[0]);
+
+        if (read(p1[0], buf, 1) > 0) 
+        {
+            // write(1, buf, 1);
+            printf("%d: received ping\n", getpid());
+        }
+        write(p2[1], "c", 1);
+
+        close(p1[0]);
+        close(p2[1]);
+        exit(0);
+    }
+    else
+    {
+        close(p1[0]);
+        close(p2[1]);
+
+        write(p1[1], "p", 1);
+
+        if (read(p2[0], buf, 1) > 0) {
+            // write(1, buf, 1);
+            printf("%d: received pong\n", getpid());
+        }
+
+        close(p1[1]);
+        close(p2[0]);
+        wait(0);
+        exit(0);
+    }
+}
